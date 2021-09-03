0a6205361f5765f0516ed50fa44c5a2654c76897
Cortlynd101 <CortlyndCox@gmail.com>
2021-09-02 19:03:12 -0600

    Okay I've fixed a lot of problems now I think.

---
+++ Student.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Student : Person
+{
+    public Student(string name, int id, decimal credits, decimal gPA)
+    {
+        Name = name;
+        ID = id;
+        Credits = credits;
+        GPA = gPA;
+    }
+    public int ID { get; set; }
+    public decimal Credits { get; set; }
+    public decimal GPA { get; set; }
+}
